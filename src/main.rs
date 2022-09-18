a36ed3e7e14261ba0cb435b76f506d8fcac8c581
Robson Santos <robfds@gmail.com>
2022-09-17 21:55:41 -0300

    Defining the basic game structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![warn(clippy::pedantic)]
+
 use bracket_lib::prelude::*;
 
 enum GameMode {
@@ -16,12 +18,54 @@ impl State {
             mode: GameMode::Menu,
         }
     }
+
+    fn main_menu(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print_centered(5, "Welcome to Flappy Dragon");
+        ctx.print_centered(8, "(P) Play Game");
+        ctx.print_centered(9, "(Q) Quit Game");
+
+        if let Some(key) = ctx.key {
+            match key {
+                VirtualKeyCode::P => self.restart(),
+                VirtualKeyCode::Q => ctx.quitting = true,
+                _ => {}
+            }
+        }
+    }
+
+    fn dead(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print_centered(5, "You're dead");
+        ctx.print_centered(8, "(P) Play Again");
+        ctx.print_centered(9, "(Q) Quit Game");
+
+        if let Some(key) = ctx.key {
+            match key {
+                VirtualKeyCode::P => self.restart(),
+                VirtualKeyCode::Q => ctx.quitting = true,
+                _ => {}
+            }
+        }
+    }
+
+    fn play(&mut self, _ctx: &mut BTerm) {
+        // TODO: fill in this stub later
+        self.mode = GameMode::End;
+    }
+
+    fn restart(&mut self) {
+        self.mode = GameMode::Playing;
+    }
 }
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
-       ctx.cls();
-       ctx.print(1, 1, "Hello Flappy... Let's begin");
+        match self.mode {
+            GameMode::Menu => self.main_menu(ctx),
+            GameMode::End => self.dead(ctx),
+            GameMode::Playing => self.play(ctx),
+        }
     }
 }
 
@@ -30,5 +74,5 @@ fn main() -> BError {
         .with_title("Flappy Dragon")
         .build()?;
 
-        main_loop(context, State::new())
+    main_loop(context, State::new())
 }
